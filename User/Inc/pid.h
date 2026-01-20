0157afa54e982320fc89f9036af8457fc00dec90
BOX268 <119125853+BOX268@users.noreply.github.com>
2026-01-20 19:26:13 +0100

    - updated PID

--- User/Inc/pid.h
+++ User/Inc/pid.h
@@ -1,23 +1,38 @@
 # ifndef __PID_H
 # define __PID_H
 
-/*
+/**
+ * @file pid.h
+ * 
+ * @brief PID with all the needed extra features
+ */
 
-*/
+
+ /**
+  * @brief Settings for the PID
+  */
 struct PidSettings
 {
+	// the coefficients
 	float kp, ki, kd;
 
-	float iMin, iMax;
-	float min, max;
+	// the limit of the contribution of each coefficient
+	float p_lim, i_lim, k_lim;
+	
+	// the absolute maximum value of the PID's output
+	float max_output;
+	// the absolute minimum value of the PID's output
+	float min_output;
 
 	// The ratio used to filter the derivative (the higher the more smoothing)
 	float fratio;
+	
 };
 
-/*
 
-*/
+/**
+ * @brief All the variables representing the current state of a PID
+ */
 struct PidRuntime
 {
 	/** The total accumulated integral, after applying the coef */
@@ -27,11 +42,30 @@ struct PidRuntime
 	float d;
 
 	/** The output of the PID the last time it was ran */
-	float lastVal;
+	float last_val;
 };
 
 
-float PID_Run(struct PidRuntime *runtime, const struct PidSettings *settings, float currentVal, float targetVal);
+/**
+ * @brief Resets all of the runtime values to default.
+ */
+void PID_reset_runtime(struct PidRuntime *runtime);
+
+
+/**
+ * @brief Manually sets the last value
+ */
+void PID_set_runtime_last_value(struct PidRuntime *runtime, float last_val);
+
+
+/**
+ * @brief runs the PID.
+ * 
+ * @note The runtine struct will be updated.
+ * 
+ * @param delta The delta time since the last PID run.
+ */
+float PID_Run(struct PidRuntime *runtime, const struct PidSettings *settings, float current_val, float target_val, float delta);
 
 
 # endif // __PID_H
--- User/Src/pid.c
+++ User/Src/pid.c
@@ -1,31 +1,49 @@
 # include "pid.h"
 
-# define BYPASS_PID
 
-float PID_Run(struct PidRuntime *runtime, const struct PidSettings *settings, float currentVal, float targetVal)
+void PID_reset_runtime(struct PidRuntime *runtime)
+{
+	runtime->d = 0;
+	runtime->i = 0;
+
+	runtime->last_val = 0;
+}
+
+
+void PID_set_runtime_last_value(struct PidRuntime *runtime, float last_val)
+{
+	runtime->last_val = last_val;
+}
+
+
+float PID_Run(struct PidRuntime *runtime, const struct PidSettings *settings, float current_val, float target_val, float delta)
 {
 	float p;
 	float out;
 
-	p = targetVal - currentVal;
+	p = target_val - current_val;
 
 	/** filtered derivative */
-	runtime->d = (runtime->lastVal - currentVal) * (1 - settings->fratio)
+	runtime->d = (runtime->last_val - current_val) * delta * (1 - settings->fratio)
 				+ runtime->d * settings->fratio;
 
-	runtime->i = runtime->i + p * settings->ki;
+	runtime->i = runtime->i + p * settings->ki * delta;
 
 	// clamp the integral
-	if (runtime->i < settings->iMin) runtime->i = settings->iMin;
-	if (runtime->i > settings->iMax) runtime->i = settings->iMax;
+	if (runtime->i < -settings->i_lim) runtime->i = -settings->i_lim;
+	if (runtime->i > settings->i_lim) runtime->i = settings->i_lim;
 
 	// calculates the output
 	out = p * settings->kp + runtime->i + runtime->d * settings->kd;
-	runtime->lastVal = out;
 
 	// clamp the final output
-	if (out > settings->max) out = settings->max;
-	else if (out < settings->min) out = settings->min;
+	if (out > settings->max_output) out = settings->max_output;
+	else if (out < -settings->max_output) out = -settings->max_output;
+
+	if (out >= 0 && out < settings->min_output) out = settings->min_output;
+	else if (out <= 0 && out > -settings->min_output) out = -settings->min_output;
+
+	runtime->last_val = out;
 
 	return out;
 }
