81aaa3183739063275f02e6b85ba9c53d08519de
BOX_268 <119125853+BOX268@users.noreply.github.com>
2026-02-10 13:32:22 +0100

    - added new statemachine code

---
+++ User/Inc/mv_statemachine.h
@@ -0,0 +1,111 @@
+#ifndef __MOVEMENT_STATEMACHINE_H
+#define __MOVEMENT_STATEMACHINE_H
+
+/**
+  * @file mv_statemachine.h
+  *
+  * @brief The statemachine used for movement control
+  */
+
+/**
+  * @brief The queue size for the statemachine
+  */
+# define MV_STATEMACHINE_LENGTH 4
+
+
+// early declarations
+struct MvStateMachine;
+struct MvStateEnv;
+
+
+
+typedef unsigned int uint_t;
+typedef int int_t;
+
+typedef void (*MvStateCall)(struct MvStateMachine*, struct MvStateEnv*, float delta_time);
+
+
+
+struct MvStateEnv
+{
+	int dummy;	
+};
+
+
+
+struct MvState
+{
+	MvStateCall wake;
+	MvStateCall run;
+	MvStateCall stop;	
+};
+
+
+
+struct MvStateMachine
+{
+	struct MvState* state_queue[MV_STATEMACHINE_LENGTH+1];
+	struct MvStateEnv envs_queue[MV_STATEMACHINE_LENGTH+1];
+	uint_t index;
+	int_t state_finished;
+
+	struct MvState* construction_state_queue[MV_STATEMACHINE_LENGTH];
+	struct MvStateEnv construction_envs_queue[MV_STATEMACHINE_LENGTH];
+	uint_t construction_index;
+	uint_t load_construction_queue;
+};
+
+
+
+/**
+  * @brief Always use this function to initialize a new statemachine struct.
+  * It will initialize some fields of the struct.
+  * @return The initialized struct.
+  */
+struct MvStateMachine DSM_init();
+
+/**
+  * @brief Empties the construction states queue, allowing it to be redefined from the start.
+  * @return 0
+  */
+int_t MSM_reset_construction(struct MvStateMachine *machine);
+
+/**
+  * @brief Put this state and the associated environment to the construction queue.
+  * @return 0 if successful, -1 id the queue is full.
+  */
+int_t MSM_enqueue_state(struct MvStateMachine *machine, struct MvState *state, struct MvStateEnv env);
+
+/**
+  * @brief Flags the construction queue as ready to be loaded. It will be moved to the active queue the next time the statemachine is updated.
+  * @return 0
+  */
+int_t MSM_ready_construction(struct MvStateMachine *machine);
+
+/**
+  * @brief Get the index where the next state we add to the construction queue will be put.
+  * @return The current construction index.
+  */
+uint_t MSM_get_construction_index(struct MvStateMachine *machine);
+
+/**
+  * @brief Check if the construction queue is full.
+  * @return 1 if the construction queue is full, 0 otherwise.
+  */
+int_t MSM_is_construction_full(struct MvStateMachine *machine);
+
+/**
+  * @brief Check if the statemachine is still executing any state from the queue.
+  * @return 1 if the statemachine is busy, 0 otherwise.
+  */
+int_t MSM_is_busy(struct MvStateMachine *machine);
+
+/**
+  * @brief Put this state and the associated environment to the construction queue.
+  * @return 0
+  */
+int_t MSM_update(struct MvStateMachine *machine, float delta_time);
+
+
+
+#endif
---
+++ User/Src/mv_statemachine.c
@@ -0,0 +1,118 @@
+#include "mv_statemachine.h"
+
+#include <stdio.h>
+
+
+
+struct MvStateMachine MSM_init()
+{
+	struct MvStateMachine machine =
+	{
+		.index = 0,
+		.construction_index = 0,
+		.load_construction_queue = 0,
+	};
+
+	machine.state_queue[MV_STATEMACHINE_LENGTH] = 0;
+
+	return machine;
+}
+
+int_t MSM_reset_schedule(struct MvStateMachine *machine)
+{
+	machine->construction_index = 0;
+	return 0;
+}
+
+int_t MSM_enqueue_state(struct MvStateMachine *machine, struct MvState *state, struct MvStateEnv env)
+{
+	if (MSM_is_construction_full(machine)) return -1;
+
+	machine->construction_state_queue[machine->construction_index] = state;
+	machine->construction_envs_queue[machine->construction_index] = env;
+	machine->construction_index ++;
+
+	return 0;
+}
+
+int_t MSM_ready_schedule(struct MvStateMachine *machine)
+{
+	machine->load_construction_queue = 1;
+	return 0;
+}
+
+uint_t MSM_get_schedule_index(struct MvStateMachine *machine)
+{
+	return machine->construction_index;
+}
+
+int_t MSM_is_schedule_full(struct MvStateMachine *machine)
+{
+	return machine->construction_index >= MV_STATEMACHINE_LENGTH;
+}
+
+int MSM_is_busy(struct MvStateMachine *machine)
+{
+	return machine->index == MV_STATEMACHINE_LENGTH;
+}
+
+int MSM_update(struct MvStateMachine *machine, float delta_time)
+{
+	struct MvState *current_state = machine->state_queue[machine->index];
+	struct MvStateEnv *current_env = &machine->envs_queue[machine->index];
+
+	// tests if a new state sequence is ready to be loaded in
+	if (machine->load_construction_queue)
+	{
+		machine->load_construction_queue = 0;
+	
+		// exits the current state
+		if (current_state != 0) if (current_state->stop != 0) current_state->stop(machine, current_env, delta_time);
+		// loads the construction state queue as the current state queue
+		uint_t i = 0;
+		for (i = 0; i < machine->construction_index; i++)
+		{
+			machine->state_queue[i] = machine->construction_state_queue[i];
+		}
+		for (; i < MV_STATEMACHINE_LENGTH; i++)
+		{
+			machine->state_queue[i] = 0;
+		}
+		machine->index = 0;
+		machine->state_finished = 0;
+
+		// updates the current state and env
+		current_state = machine->state_queue[machine->index];
+		current_env = &machine->envs_queue[machine->index];
+
+
+		// wakes up the new state
+		if (current_state != 0) if (current_state->stop != 0) current_state->wake(machine, current_env, delta_time);
+	}
+
+	// test if the currently active state has finished
+	if (machine->state_finished == 1)
+	{
+		// exits the current state
+		if (current_state != 0) if (current_state->stop != 0) current_state->stop(machine, current_env, delta_time);
+
+		if (machine->index < MV_STATEMACHINE_LENGTH)
+		{
+			machine->index ++;
+
+			// updates the current state and env
+			current_state = machine->state_queue[machine->index];
+			current_env = &machine->envs_queue[machine->index];
+
+			// wakes up the current state
+			if (current_state != 0) if (current_state->stop != 0) current_state->wake(machine, current_env, delta_time);
+
+			machine->state_finished = 0;
+		}
+
+	}
+
+	if (current_state != 0) current_state->run(machine, current_env, delta_time);
+
+	return 0;
+}
