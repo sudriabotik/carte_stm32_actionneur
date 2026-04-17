d336bad1033209b80290452448b7cd92429cd48b
Paul <vitalium.02@gmail.com>
2026-04-17 03:47:21 +0200

    respect u gitignore

--- STM32CubeIDE/Debug/Src/coordonne_absolue.d
+++
@@ -1,9 +0,0 @@
-Src/coordonne_absolue.o: \
- C:/Users/PAUL/OneDrive\ -\ ESME/Documents/botique/2026/info/stm32_project/stm32_carte_actionneur/carte_stm32_actionneur/User/Src/coordonne_absolue.c \
- ../../User/Inc/coordonne_absolue.h ../../User/Inc/mv_statemachine.h \
- ../../lib_robot/inc_lib/mathfuncs.h \
- ../../User/Inc/mv_statemachine_states.h
-../../User/Inc/coordonne_absolue.h:
-../../User/Inc/mv_statemachine.h:
-../../lib_robot/inc_lib/mathfuncs.h:
-../../User/Inc/mv_statemachine_states.h:
--- STM32CubeIDE/Debug/src_lib/mathfuncs.d
+++
@@ -1,4 +0,0 @@
-src_lib/mathfuncs.o: \
- C:/Users/PAUL/OneDrive\ -\ ESME/Documents/botique/2026/info/stm32_project/stm32_carte_actionneur/carte_stm32_actionneur/lib_robot/src_lib/mathfuncs.c \
- ../../lib_robot/inc_lib/mathfuncs.h
-../../lib_robot/inc_lib/mathfuncs.h:
